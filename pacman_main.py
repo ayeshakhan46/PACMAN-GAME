623633d91bbb4b34b9a1e1397b7e692ff94385ff
HP <midhaahsan896@gmail.com>
2023-09-03 23:13:50 +0500

    draw board code.

--- pacman_main.py
+++ pacman_main.py
@@ -1,5 +1,6 @@
 import pygame
 import os, sys
+import math
 
 
 #create display of pacman game
@@ -46,6 +47,8 @@ pygame.display.set_caption("PACMAN GAME")
 timer = pygame.time.Clock()
 fps=60
 levels = boards
+colour= 'blue'
+PI = math.pi
 # ttf_path = os.path.join(sys.path[0], "freesanbold.ttf")
 # pygame.font.Font(ttf_path, 20)
 # font= pygame.font.Font('',20)
@@ -58,3 +61,36 @@ while run:
             run=False
     pygame.display.flip()
 pygame.quit()
+
+
+def draw_board():
+    num1=((screen_height-50)//32)
+    num2=(screen_width//30)
+    for i in  range(len(levels)):
+        for j  in range(len(levels[i])):
+            if levels[i][j]==1:
+                pygame.draw.circle(screen , 'white' , (j*num2 + (0.5*num2), i*num1 +(0.5*num1)),4)
+            if levels[i][j] == 2:
+                pygame.draw.circle(screen, 'white', (j * num2 + (0.5 * num2), i * num1 + (0.5 * num1)), 10)
+            if levels[i][j] == 3:
+                pygame.draw.line(screen, colour, (j * num2 + (0.5 * num2), i * num1 ),
+                                 (j * num2 + (0.5 * num2), i * num1 + num1),3)
+            if levels[i][j] == 4:
+                pygame.draw.line(screen, colour, (j * num2, i * num1 + (0.5 * num1)),
+                                 (j * num2 + num2, i * num1 + (0.5 * num1)), 3)
+            if levels[i][j] == 5:
+                pygame.draw.arc(screen, colour, [(j * num2 - (num2 * 0.4)) - 2, (i * num1 + (0.5 * num1)), num2, num1],
+                                0, PI / 2, 3)
+            if levels[i][j] == 6:
+                pygame.draw.arc(screen, colour,
+                                [(j * num2 + (num2 * 0.5)), (i * num1 + (0.5 * num1)), num2, num1], PI / 2, PI, 3)
+            if levels[i][j] == 7:
+                pygame.draw.arc(screen, colour, [(j * num2 + (num2 * 0.5)), (i * num1 - (0.4 * num1)), num2, num1], PI,
+                                3 * PI / 2, 3)
+            if levels[i][j] == 8:
+                pygame.draw.arc(screen, colour,
+                                [(j * num2 - (num2 * 0.4)) - 2, (i * num1 - (0.4 * num1)), num2, num1], 3 * PI / 2,
+                                2 * PI, 3)
+            if levels[i][j] == 9:
+                pygame.draw.line(screen, 'white', (j * num2, i * num1 + (0.5 * num1)),
+                                 (j * num2 + num2, i * num1 + (0.5 * num1)), 3)
